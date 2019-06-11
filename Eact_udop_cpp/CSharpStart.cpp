c0432ffdc05be3e155f62ecb99cd57b4ba29d44a
suifengsigan <1075087594@qq.com>
2019-06-11 14:32:05 +0800

    优化

--- Eact_udop_cpp/CSharpStart.cpp
+++ Eact_udop_cpp/CSharpStart.cpp
@@ -3,7 +3,6 @@
 #include "stdafx.h"
 
 #include "CSharpStart.h"
-#include "AssemblyDynamicLoader.h"
 
 
 using namespace System;
@@ -16,48 +15,4 @@ char* EACT_COMMAND(int type, char* command)
 {
 	String ^ commandStr = Marshal::PtrToStringAnsi((IntPtr)command);
 	return (char*)Marshal::StringToHGlobalAnsi(EactCommand::Entry::ExcuteCommand(type, commandStr)).ToPointer();
-}
-
-char* Show(char* actionName)
-{
-	char* result = "";
-	try
-	{
-		String ^ actionNameStr = Marshal::PtrToStringAnsi((IntPtr)actionName);
-		auto path = Path::GetDirectoryName(Assembly::GetExecutingAssembly()->Location);
-		//获取上级目录
-		path = path->Substring(0, path->LastIndexOf("\\"));
-		path = Path::Combine(path,"Application");
-		ProxyObject::ExecuteMothod(actionNameStr, path);
-	}
-	catch (Exception^ ex)
-	{
-		result = (char*)Marshal::StringToHGlobalAnsi(ex->Message).ToPointer();
-	}
-	return result;
-}
-
-char* GetSYSSHAREVar(char* key)
-{
-	if (key == "IV")
-	{
-		return "1234";
-	}
-	if (key == "KEY")
-	{
-		return "1234567sdfsfsfdljlsfdkja^9999999999999";
-	}
-	if (key == "email")
-	{
-		return "577104206@qq.com";
-	}
-	if (key == "uuid")
-	{
-		return "91420100781995775H-20181030";
-	}
-	if (key == "sn")
-	{
-		return "c090381767efea67545d70e6f5cf12b2";
-	}
-	return "";
 }
--- Eact_udop_cpp/CSharpStart.h
+++ Eact_udop_cpp/CSharpStart.h
@@ -3,7 +3,3 @@
 #pragma once
 
 extern "C" __declspec(dllexport) char* Show(char* actionName);
-
-extern "C" __declspec(dllexport) char* GetSYSSHAREVar(char* key);
-
-extern "C" __declspec(dllexport) char* EACT_COMMAND(int type,char* command);
