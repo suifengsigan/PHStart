eb47378ff0aed28078e21bf3eb3b3929a4966242
suifengsigan <1075087594@qq.com>
2019-08-14 15:29:47 +0800

    优化

--- CSharpEntry/Program.cs
+++ CSharpEntry/Program.cs
@@ -21,53 +21,21 @@ namespace CSharpEntry
 
         static void Show(string[] args)
         {
-            var arg = args.Count() > 0 ? args.First() : string.Empty;
-            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
             try
             {
-                var loader = new ManagedLoader();
-                var assembly = loader.Load(arg);
-                string outArg = string.Empty;
-                int result = 0;
-                loader.Run(args.Count() > 1 ? args[1] : "Main", args, out outArg, out result);
-                #region oldCode
-                //var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(u => u.Location==Path.Combine(AppDomain.CurrentDomain.BaseDirectory,arg));
-                //Type[] types = assemblies.FirstOrDefault().GetTypes();
-                //foreach (Type type in types)
-                //{
-                //    foreach (MethodInfo info in type.GetMethods(BindingFlags.Public | BindingFlags.Static))
-                //    {
-                //        if (info.Name == "Main")
-                //        {
-                //            ParameterInfo[] parameters = info.GetParameters();
-                //            if (parameters.Length == 1)
-                //            {
-                //                Type parameterType = parameters[0].ParameterType;
-                //                Type returnType = info.ReturnType;
-                //                if ((parameterType.IsArray && (parameterType.GetElementType() == typeof(string))) && (returnType.IsArray && (returnType.GetElementType() == typeof(string))))
-                //                {
-                //                    var result = (string[])info.Invoke(null, new object[] { args });
-                //                }
-                //            }
-                //            else if(parameters.Length == 0)
-                //            {
-                //                var result = (string[])info.Invoke(null, null);
-                //            }
-                //        }
-                //    }
-                //}
-                #endregion
+                NX.Entry.GetInstance().ManagedLoader_Run(args);
             }
             catch (Exception ex)
             {
                 System.Windows.Forms.MessageBox.Show(ex.Message);
+                System.Windows.Forms.MessageBox.Show(args.ToString());
             }
         }
 
         private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
             var assemblyName = new AssemblyName(args.Name);
-            if (assemblyName.Name == "ManagedLoader")
+            if (assemblyName.Name == "ManagedLoader"||assemblyName.Name.ToUpper().Contains("NXOpen".ToUpper()))
             {
                 string fileName = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
                 var dir = System.IO.Path.GetDirectoryName(fileName);
